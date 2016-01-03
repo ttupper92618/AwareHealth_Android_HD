0b65ea96e181effa190297c5f6f1d6b40a915f17
Tom Tupper <ttupper@AWP-IRV-0113L.local>
2016-01-03 11:39:44 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ttupper/awarehealthhd/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.ttupper.awarehealthhd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+    private static final String TAG = "Main";
+    public ProgressDialog progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // webView
+        this.webview = (WebView)findViewById(R.id.awWebView);
+
+        WebSettings settings = this.webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setMinimumFontSize(1);
+
+        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        progressBar = ProgressDialog.show(MainActivity.this, "", "Loading resources...");
+
+        webview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // progressBar = ProgressDialog.show(MainActivity.this, "AwareHealth 1.0...", "Loading...");
+                Log.i(TAG, "Processing webview url click...");
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "Finished loading URL: " + url);
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+
+                }
+
+            }
+
+        });
+
+        webview.setWebChromeClient(new WebChromeClient() {
+
+
+        });
+
+        webview.loadUrl("http://ucsf.aware360aui.dev.awarepoint.com");
+
+    }
+
+}
